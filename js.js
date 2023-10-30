7091aef723804d4d828e5037df017f719d3084ff
lorenzo <lor.abbadessa@gmail.com>
2023-10-30 17:51:57 +0100

    first commit

---
+++ js.js
@@ -0,0 +1,28 @@
+for ( var i = 1 ; i <= 100 ; i++ ) {
+    if(i % 3 === 0) {
+        document.write(i + "<br>");
+    }
+}
+for ( var i = 1 ; i <= 100 ; i++ ) {
+    if(i % 5 === 0) {
+        document.write(i + "<br>");
+    }
+}
+
+
+
+
+for (let i = 0; i <= 100; i++) {
+
+
+    console.log(i)
+}
+
+
+if(i % 3 === 0) {
+    document.write(i + "<br>");
+}else(i % 5 === 0) {
+    document.write(i + "<br>"); 
+}
+
+
